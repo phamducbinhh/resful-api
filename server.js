2d646bcfa6ce2cdee51a151a2ec16ddc7a112f4a
phamducbinhh <phamducbinh1712000@gmail.com>
2022-09-09 12:54:13 +0700

    update

--- server.js
+++ server.js
@@ -22,10 +22,18 @@ const app = express();
 app.use(
   cors({
     origin: "http://localhost:5000",
+    methods: "GET,PUT,POST,DELETE",
   })
 );
 //express.json()là một phương thức được tích hợp sẵn để nhận ra Đối tượng Yêu cầu đến là một Đối tượng JSON . Phương thức này được gọi là phần mềm trung gian trong ứng dụng của bạn bằng cách sử dụng mã:app.use(express.json());
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 //express.urlencoded()là một phương thức được xây dựng sẵn để nhận ra Đối tượng Yêu cầu đến dưới dạng chuỗi hoặc mảng . Phương thức này được gọi là phần mềm trung gian trong ứng dụng của bạn bằng cách sử dụng mã:app.use(express.urlencoded());
 app.use(express.urlencoded({ extended: true }));
 //api khi thanh toán bằng paypal
